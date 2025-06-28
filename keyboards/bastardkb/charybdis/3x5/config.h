4090efb6451e1758f77e7946f32586080f19aea5
Kamil Zwoinski <kamilzwoinski@gmail.com>
2025-06-28 13:18:45 +0200

    re-add accidentally deleted lines

--- keyboards/bastardkb/charybdis/3x5/config.h
+++ keyboards/bastardkb/charybdis/3x5/config.h
@@ -25,3 +25,22 @@
 // #define ROTATIONAL_TRANSFORM_ANGLE -25
 // For left-handed
 #define ROTATIONAL_TRANSFORM_ANGLE 40
+
+// To use the handedness pin, resistors need to be installed on the adapter PCB.
+// If so, uncomment the following code, and undefine MASTER_RIGHT above.
+// #define SPLIT_HAND_PIN GP15
+// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
+
+/* VBUS detection. */
+#define USB_VBUS_PIN GP19
+
+/* SPI & PMW3360 settings. */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP22
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16
+
+/* Reset. */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
